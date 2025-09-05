149501ce257873b9dc9f23829447ea328546173b
Fayez Morsel <fayezmorsel77@gmail.com>
2025-09-05 13:44:24 +0300

    add alert button and message and close button

--- tutorial-react/src/App.tsx
+++ tutorial-react/src/App.tsx
@@ -1,10 +1,15 @@
 import Button from "./components/Button";
+import Alert from "./components/Alert";
+import { useState } from "react";
 
 
- function App() {
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+
   return (
     <div>
-      <Button onClick={() => console.log('Clicked')}>My Button </Button>
+      { alertVisible && <Alert onClose={() => setAlertVisibility(false)}>My alert</Alert>}
+      <Button color="primary" onClick={() => setAlertVisibility(true)}>My Button </Button>
     </div>
   );
 }
