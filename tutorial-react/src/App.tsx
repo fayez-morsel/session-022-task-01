3cc1effd467dfc878e3d5626c09e822d1542adef
Fayez Morsel <fayezmorsel77@gmail.com>
2025-09-05 13:25:30 +0300

    add new components button

--- tutorial-react/src/App.tsx
+++ tutorial-react/src/App.tsx
@@ -1,11 +1,10 @@
-import Alert from "./components/Alert";
+import Button from "./components/Button";
+
 
  function App() {
   return (
     <div>
-      <Alert>
-        Hello World<span>Fayez</span>
-      </Alert>
+      <Button onClick={() => console.log('Clicked')}>My Button </Button>
     </div>
   );
 }
